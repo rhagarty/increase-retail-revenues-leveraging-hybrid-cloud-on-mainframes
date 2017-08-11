5dee9e862f308fcc175b2ee7f06a7653fc603fe4
Eva Yan <evalyan@us.ibm.com>
2017-08-11 15:47:34 -0400

    Remove duplicated directory

--- Sample code/vsl/vslrecws/vcap-local.js
+++
@@ -1,7 +0,0 @@
-// This module helps to run the application in local development environment
-// by faking CloudFoundry's VCAP_SERVICES environment variable
-process.env.VCAP_SERVICES = process.env.VCAP_SERVICES || JSON.stringify({
-
-
-});
-
